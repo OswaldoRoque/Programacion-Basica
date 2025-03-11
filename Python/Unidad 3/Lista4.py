ad92052bbfd01250a4c9a9b2075669259dbb6982
OswaldoRoque <oswaldobenjaminroquecastillo@gmail.com>
2025-03-11 08:00:45 -0600

    11 de marzo

---
+++ Python/Unidad 3/Lista4.py
@@ -0,0 +1,42 @@
+# Lista4
+Nombre = ["Aaron","Adan","Sabaid","Victor Manuel","Lupita","Victor Alejandro","Enrique","Arath","Alexa","Roberto","Oswaldo"]
+Edades = ["18","18","18","18","18","18","18","18","18","18","18"]
+Libre = ["Alan Pulido","Javier Hernandez","Roberto Alvarado","Alan Moso","Hugo Camberos","Raul Rangel",""]
+Materias = ["Programacion basica","Calculo integral","Contabilidad","Ingles","Ingenieria de los materiales","Algebraa lineal","Estadistica"]
+Profesores = ["Eduardo","Elizabeth","Martha","Marlen","Enrique","Jorge","Guillermo"]
+
+#Se debe de ejecutar los nombres 
+print(Nombre)
+
+#Aqui debe ejecutar la lista de edades 
+print(Edades)
+
+#Deberia de imprimir la lista libre
+print(Libre)
+
+#Aqui se imprime la lista de materias
+print(Materias)
+
+#En esta se imprime la lista de los profes
+print(Profesores)
+
+#En este se debe de imprimir el numero de nombres que estan en la lista
+print(len(Nombre))
+
+#En este debe imprimirse el numero maximo
+print(max(Edades))
+
+#En este debe de imprimirse el numero minimo
+print(min(Edades))
+
+#En este tiene que acomodarse por letra del abecedario 
+Nombre.sort()
+print(Nombre)
+
+#En este codigo debe de eliminarse programacion basica de la lista
+Materias.remove("Programacion basica")
+print(Materias)
+
+#Aqui debe de agregarse y el 1 es para la posicion
+Materias.append((1),"Programacion avanzada")
+print(Materias)
